62bbab8593178f7f389f69ad8b41644cbfbc1d37
ygormendes <ygormendesx@gmail.com>
2023-08-31 01:14:09 -0300

    main responsive

--- src/pages/homepage/main.js
+++ src/pages/homepage/main.js
@@ -8,7 +8,7 @@ const HomePage = () => {
       <div className="background">
         <img src={main} alt="Background" className="background-gif" />
       </div>
-      <div className="header-main d-flex justify-content-between align-items-center">
+      <div className="header-main d-flex align-items-center">
         <div className="d-flex flex-column custom-link align-items-sm-center align-items-center align-items-md-start align-items-lg-start align-items-xl-start page-title">
           <a href="/" className="home-title-play-an">
             PLAYMAN
@@ -17,8 +17,8 @@ const HomePage = () => {
             Generative Artist
           </a>
         </div>
-        <div className="list-menu">
-          <ul className="list-menu d-flex align-items-center justify-content-between custom-link modern-menu">
+        <div className="list-menu d-flex justify-content-center">
+          <ul className="list-menu d-flex align-items-center justify-content-center custom-link modern-menu">
             <li>
               <a href="./about">About</a>
             </li>
